a9b1b19a7054ade5e14a8eb19b2ac5109d36bd59
Salil2005 <salilgupta2005@gmail.com>
2026-07-13 17:24:34 +0530

    sync Stream

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,11 +1,11 @@
 import {StreamChat} from 'stream-chat';
 import {ENV} from './env.js'; 
 
-const apiKey = ENV.KEYSTREAM_API_;
+const apiKey = ENV.KEYSTREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
 if(!apiKey || !apiSecret){
-    console.log("KEYSTREAM_API_ or STREAM_API_SECRET is missing in the environment variables");
+    console.log("KEYSTREAM_API_KEY or STREAM_API_SECRET is missing in the environment variables");
 }
 
 export const streamClient = StreamChat.getInstance(apiKey, apiSecret);
@@ -14,18 +14,19 @@ export const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 export const upsertStreamUser = async(userData) => {
     try{
         await streamClient.upsertUser(userData);
-        console.log("Stream user upserteed successfully :" , userData);
+        console.log("Stream user upserted successfully :" , userData);
         
     } catch (error) {
         console.log("Error upserting user to Stream Chat:", error);
+        throw error;
         
     }
 }
 
 export const deleteStreamUser = async(userData) => {
     try{
-        await streamClient.deleteUser(userId);
-        console.log("User deleted successfully " , userId);
+        await streamClient.deleteUser(userData.id);
+        console.log("User deleted successfully " , userData.id);
         
     } catch (error) {
         console.log("Error deleting user from Stream Chat:", error);
